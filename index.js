c7cfc217c772bde43e79556d50f4eb91818d7664
Roundabout Rider <ntm.email@icloud.com>
2018-08-26 11:30:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 var recipes = { object : 'key' }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  Object.assign({},object,{[key]:value});
-  return recipes
+  return Object.assign({},object,{[key]:value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+   recipes[key]=value;
+   return recipes;
+}
+function deleteFromObjectByKey(recipes, key){
+   const simo= Object.assign({}, recipes);
+   delete simo[key];
+   return simo;
+ }
+
+ function destructivelyDeleteFromObjectByKey(recipes, key){
+   delete recipes[key];
+   return recipes;
+ }
